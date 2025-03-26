df079819bc1b316a94957556e4ee4acc2401a398
Abdul Adzis Abpet <aaAbpet@mcm.edu.ph>
2025-03-26 11:59:55 +0800

    Tutorial SFX
    Added Voice overs for tutorials

--- Assets/_Scripts/In Game HUD/DialogueSequence.cs
+++ Assets/_Scripts/In Game HUD/DialogueSequence.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Dialogue", menuName = "ScriptableObjects/Dialogue")]
 public class DialogueSequence : ScriptableObject
 {
-    [SerializeField, Tooltip("List of dialogue groups that make up this sequence")]
-    private List<DialogueLineGroup> _dialogue = new List<DialogueLineGroup>();
+    [SerializeField, Tooltip("List of dialogue groups that make up this sequence")] private AudioClip voiceOver;
+    [SerializeField] private List<DialogueLineGroup> _dialogue = new List<DialogueLineGroup>();
 
     public List<DialogueLineGroup> Dialogue => _dialogue;
+    public AudioClip VoiceOver => voiceOver;
 }
